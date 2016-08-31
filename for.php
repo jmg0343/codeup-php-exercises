8bb7da041ad7af7ad1aa85ba914a37538ce4c93b
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-08-31 10:45:57 -0500

    updated For Loop exercise

--- for.php
+++ for.php
@@ -3,8 +3,13 @@
 	$startingNumber = (int)fgets(STDIN);
 	fwrite(STDOUT, "Please enter an ending number\n");
 	$endingNumber = (int)fgets(STDIN);
+	fwrite(STDOUT, "Increment by how much?");
+	$incrementCount= (int)fgets(STDIN);
 
-	for ($i = $startingNumber; $i <= $endingNumber; $i++) {
+	for ($i = $startingNumber; $i <= $endingNumber; $i += $incrementCount) {
 		echo $i . PHP_EOL;
+		if ($incrementCount == "") {
+			$incrementCount = 1;
+		}
 	}
  ?>
