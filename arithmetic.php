512f12149388984f04831cf828bc5f9e03f6287d
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-01 14:33:15 -0500

    updated arithmetic exercise with customized error message functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,37 +1,80 @@
 <?php
 
-$a = 10;
-$b = 5;
+fwrite(STDOUT, "1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for modulus\n");
+$operation = trim(fgets(STDOUT));
 
-function add($a, $b)
-{
-    return $a + $b;
+fwrite(STDOUT, "Please enter first number\n");
+$a = trim(fgets(STDOUT));
+fwrite(STDOUT, "Please enter second number\n");
+$b = trim(fgets(STDOUT));
+
+function errorMessage() {
+	echo "Error: Both inputs must be numbers";
+}
+function cannotDivideByZero() {
+	echo "Cannot Divide By 0";
 }
 
-function subtract($a, $b)
-{
-    return $a - $b;
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;		
+	} else {
+		return errorMessage();
+	}
 }
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function subtract($a, $b) {
+   if(is_numeric($a) && is_numeric($b)) {
+   		return $a - $b;
+   } else {
+   		return errorMessage();
+   }
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function multiply($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b;		
+	} else {
+		return errorMessage();
+	}
 }
 
-function modulus($a, $b)
-{
-	return $a % $b;
+function divide($a, $b) {
+	if ($b == 0) {
+		return cannotDivideByZero();
+	} elseif (is_numeric($a) && is_numeric($b)) {
+    	return $a / $b;		
+	} else {
+		return errorMessage();
+	}
 }
 
-// echo add(5, 4) . PHP_EOL;
-// echo subtract(10, 5) . PHP_EOL;
-// echo multiply(3, 5) . PHP_EOL;
-// echo divide(9, 3) . PHP_EOL;
-// echo modulus(6, 2) . PHP_EOL;
+function modulus($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a % $b;		
+	} else {
+		return errorMessage();
+	}
+}
+	
+switch ($operation) {
+	case '1':
+		echo add($a, $b) . PHP_EOL;
+		break;
+	case '2':
+		echo subtract($a, $b) . PHP_EOL;
+		break;
+	case '3':
+		echo multiply($a, $b) . PHP_EOL;
+		break;
+	case '4':
+		echo divide($a, $b) . PHP_EOL;
+		break;
+	case '5':
+		echo modulus($a, $b) . PHP_EOL;
+		break;
+	default:
+		echo "Please enter something useful";
+		break;
+}
 
-echo add($a, $b) . PHP_EOL;
