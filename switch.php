365c5f3e867df5e02589e5bdcb7b53395356bcef
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-01 10:58:58 -0500

    update to exercise allowing the use of day() function

--- switch.php
+++ switch.php
@@ -33,27 +33,27 @@
 	         break;
  	}
 
- // 	fwrite(STDOUT, "Enter day of the week. 1 (for Monday) through 7 (for Sunday)");
-	// $dayAsNumber = trim(fgets(STDIN));
+ 	fwrite(STDOUT, "Enter day of the week. 1 (for Monday) through 7 (for Sunday)");
+	$dayAsNumber = trim(fgets(STDIN));
 
-	// if ($dayAsNumber == 1) {
-	// 	echo "Monday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 2) {
-	// 	echo "Tuesday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 3) {
-	// 	echo "Wednesday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 4) {
-	// 	echo "Thursday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 5) {
-	// 	echo "Friday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 6) {
-	// 	echo "Saturday" . PHP_EOL;
-	// }
-	// if ($dayAsNumber == 7) {
-	// 	echo "Sunday" . PHP_EOL;
-	// }
+	if ($dayAsNumber == 1) {
+		echo "Monday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 2) {
+		echo "Tuesday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 3) {
+		echo "Wednesday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 4) {
+		echo "Thursday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 5) {
+		echo "Friday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 6) {
+		echo "Saturday" . PHP_EOL;
+	}
+	if ($dayAsNumber == 7) {
+		echo "Sunday" . PHP_EOL;
+	}
