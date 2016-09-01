1b69e2f3fff5f74a2b38d66b2794d4088d58739c
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-09-01 09:30:04 -0500

    added for loop that uses break to stop counting after 10

--- break_continue.php
+++ break_continue.php
@@ -1,10 +1,20 @@
 <?php 
-	for ($i = 1; $i <= 100; $i ++) {
+	for ($i = 1; $i <= 100; $i++) {
 		if ($i % 2 == 1) {
 			continue;
 		} else {
 			echo $i . PHP_EOL;
 		}
 	} 
+
+	echo PHP_EOL;
+
+	for ($i = 1; $i <= 100; $i++) {
+		if ($i == 11) {
+			break;
+		} else {
+			echo $i . PHP_EOL;
+		}
+	}
 
  ?>
